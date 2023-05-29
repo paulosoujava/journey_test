fd31a9f921d967ae86016abc9ac31415c784aa6d
Paul Oliveira <paulo.jorge@intelbras.com.br>
2023-05-29 11:46:54 -0300

    first commit

--- lib/journey_test.dart
+++ lib/journey_test.dart
@@ -3,16 +3,19 @@ library journey_test;
 import 'package:flutter/material.dart';
 
 class JouneyTest extends StatelessWidget {
-  const JouneyTest({super.key});
+  final String title;
+  final String content;
+
+  const JouneyTest({super.key, required this.title, required this.content}){}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Journey'),
+          title:  Text(title),
         ),
-        body: const Center(
-          child: Text('Journey'),
+        body:  Center(
+          child: Text(content),
         ));
   }
 }
