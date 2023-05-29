34e61dd58c0ea2fd2c07a3686166cb336c90f548
Paul Oliveira <paulo.jorge@intelbras.com.br>
2023-05-29 11:12:57 -0300

    first commit

---
+++ lib/journey_test.dart
@@ -0,0 +1,18 @@
+library journey_test;
+
+import 'package:flutter/material.dart';
+
+class JouneyTest extends StatelessWidget {
+  const JouneyTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Journey'),
+        ),
+        body: const Center(
+          child: Text('Journey'),
+        ));
+  }
+}
